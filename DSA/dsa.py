253d39feada4449c583a293d40106d0238f9b361
Ayamigah16 <abkc070@gmail.com>
2023-05-05 20:57:35 +0000

    Dynamic programming through memoization

--- DSA/dsa.py
+++ DSA/dsa.py
@@ -518,3 +518,29 @@ def max(array):
     # the function has changed from O(2^n) to O(N)
 
 #print(max([1,2,3,4]))
+
+## dynamic programing through memoiazation
+###memoization is a simple, but brilliant technique for reducing recursive calls in cases of overlapping subproblems.
+
+def fib(n, memo):
+    if n == 0 or n == 1:
+        return n
+    
+    # Check the hash table (called memo) to see whether fib(n)
+    # was already computed or not:
+    if not memo.get(n):
+        # If n is NOT in memo, compute fib(n) with recursion
+        # and then store the result in the hash table:
+        memo[n] = fib(n - 2, memo) + fib(n - 1, memo) 
+       
+    # By now, fib(n)'s result is certainly in memo. (Perhaps
+    # it was there before, or perhaps we just stored it there
+    # in the previous line of code. But it's certainly there now.)
+    # So let's return it:
+    return memo[n]
+
+    # dropped from O(2^N) to O(N), ie 2N -1
+    
+    
+
+
