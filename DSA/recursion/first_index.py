41815c21260d5073aa844ea3d398d8d7e68c1b0d
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2023-06-03 22:42:07 +0000

    first index of character x

--- DSA/recursion/first_index.py
+++ DSA/recursion/first_index.py
@@ -1,5 +1,32 @@
 # QUESTION
-"""Use recursion to write a function that accepts a string and returns the
+"""
+Use recursion to write a function that accepts a string and returns the
 first index that contains the character “x.” For example, the string,
 "abcdefghijklmnopqrstuvwxyz" has an “x” at index 23. To keep things simple,
-assume the string definitely has at least one “x."""
+assume the string definitely has at least one “x."
+"""
+
+# using loop
+spam = "hahsamsa"
+def index_of_X(string):
+    if 'x' not in string:
+        return -1
+    
+    for char in string:
+        if char == "x":
+            return string.index(char)
+
+ 
+
+# using recursion
+def x_index(string):
+    # base case
+    """
+    if string has only one character ruturn 0; also string can't be empty
+    """ 
+    if string[0] == 'x':
+        return 0
+    
+    return x_index(string[1:]) + 1
+    
+print(x_index('shjx'))
