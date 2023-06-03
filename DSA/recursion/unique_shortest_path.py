70b26601ca25a4ebf3e539f9161598e7543efd65
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2023-06-03 22:48:00 +0000

    unique shortest path

--- DSA/recursion/unique_shortest_path.py
+++ DSA/recursion/unique_shortest_path.py
@@ -6,10 +6,16 @@ paths from the upper-leftmost square to the lower-rightmost square.
 
 For example, here’s what the grid looks like with three rows and seven
 columns. You want to get from the “S” (Start) to the “F” (Finish).
-[S, , , ]
-[ , , , ]
-[ , , ,F]
+[S, , , , , , ]
+[ , , , , , , ]
+[ , , , , , , F]
 By “shortest” path, I mean that at every step, you’re moving either one
 step to the right or one step downward
 Again, your function should calculate the number of shortest paths.
 """
+def unique_paths(rows, columns):
+    if rows == 1 or columns == 1:
+        return 1 
+    return unique_paths(rows - 1, columns) + unique_paths(rows, columns - 1)
+
+print(unique_paths(3,7))
