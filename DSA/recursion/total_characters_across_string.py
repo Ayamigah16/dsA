02e55b39727c0caa06f91d5362fc5ade21eed380
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2023-06-03 22:43:23 +0000

    total characters across strings

--- DSA/recursion/total_characters_across_string.py
+++ DSA/recursion/total_characters_across_string.py
@@ -3,3 +3,21 @@
 returns the total number of characters across all the strings. For example,
 if the input array is ["ab", "c", "def", "ghij"], the output should be 10 since there
 are 10 characters in total"""
+
+
+## using loop
+def total_number_of_characters(array):
+    sum = 0
+    for string in array:
+        sum = sum + len(string)
+    return sum
+
+## using recursion
+def number_of_characters(array):
+    if len(array) == 1:
+        return len(array[0])
+    
+    return len(array[0]) + number_of_characters(array[1:])
+
+
+print(number_of_characters(["ab", "c", "def", "ghij"]))
